5b2afc7f4ee59a98426fd167341d59a09b5d89b4
oussama464 <oussama.missaoui201@gmail.com>
2022-07-29 17:07:14 +0200

    add libs folder and dev requirements

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import pyspark
+import os
+import sys
+import importlib
+import argparse
+
+if os.path.exists('jobs.zip'):
+    sys.path.insert(0, 'jobs.zip')
+else:
+    sys.path.insert(0, './jobs')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--job', type=str, required=True)
+parser.add_argument('--job-args', nargs='*')
+args = parser.parse_args()
+
+sc = pyspark.SparkContext(appName=args.job_name)
+job_module = importlib.import_module('jobs.%s' % args.job)
+job_module.analyze(sc, job_args)
