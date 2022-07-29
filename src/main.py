c7779fd56023283b129a3f91887d883942f55dfc
oussama464 <oussama.missaoui201@gmail.com>
2022-07-29 20:05:20 +0200

    remaining code

--- src/main.py
+++ src/main.py
@@ -1,19 +1,43 @@
-import pyspark
-import os
 import sys
-import importlib
-import argparse
+from pyspark.sql import SparkSession
+import os
 
 if os.path.exists('jobs.zip'):
     sys.path.insert(0, 'jobs.zip')
-else:
-    sys.path.insert(0, './jobs')
+if os.path.exists('libs.zip'):
+    sys.path.insert(0, "libs.zip")
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--job', type=str, required=True)
-parser.add_argument('--job-args', nargs='*')
-args = parser.parse_args()
+from logger import Log4j
 
-sc = pyspark.SparkContext(appName=args.job_name)
-job_module = importlib.import_module('jobs.%s' % args.job)
-job_module.analyze(sc, job_args)
+
+# --conf "-Dlog4j.configuration=file:log4j.properties -Dlogfile.name=hello-spark -Dspark.yarn.app.container.log.dir=app-logs"
+
+
+def main():
+    spark = SparkSession \
+        .builder \
+        .master("yarn") \
+        .appName("HelloSparkSQL") \
+        .getOrCreate()
+
+    logger = Log4j(spark)
+
+    # if len(sys.argv) != 2:
+    #    logger.error("Usage: HelloSpark <filename>")
+    #    sys.exit(-1)
+
+    surveyDF = spark.read \
+        .option("header", "true") \
+        .option("inferSchema", "true") \
+        .csv("./data/sample.csv")
+
+    surveyDF.createOrReplaceTempView("survey_tbl")
+    countDF = spark.sql(
+        "select Country, count(1) as count from survey_tbl where Age<40 group by Country")
+
+    countDF.show()
+    logger.info(countDF.count())
+
+
+if __name__ == "__main__":
+    main()
